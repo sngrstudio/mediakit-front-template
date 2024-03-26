66aed3cff5bb7bb9f00d8abbcd0615907c1bbf91
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-03-26 23:09:55 +0700

    WIP

--- src/data/fetcher.ts
+++ src/data/fetcher.ts
@@ -1,4 +1,4 @@
-const GRAPHQL_ENDPOINT = `${import.meta.env.ADMIN_ENDPOINT}/wp/graphql`
+const GRAPHQL_ENDPOINT = `${process.env.ADMIN_ENDPOINT}/wp/graphql`
 
 export const gql = String.raw
 export const fetchData = async ({ query, variables }: { query: ReturnType<typeof gql>; variables?: Record<string, string> }) => await fetch(GRAPHQL_ENDPOINT, {
