de837166d35e9f1d435fd732b8b710c0fa1a3040
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-03-26 09:36:11 +0000

    WIP

---
+++ src/data/fetcher.ts
@@ -0,0 +1,11 @@
+const GRAPHQL_ENDPOINT = `${import.meta.env.ADMIN_ENDPOINT}/wp/graphql`
+
+export const gql = String.raw
+export const fetchData = async ({ query, variables }: { query: ReturnType<typeof gql>; variables?: Record<string, string> }) => await fetch(GRAPHQL_ENDPOINT, {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+        query,
+        variables
+    })
+})
---
+++ src/data/posts.ts
@@ -0,0 +1,55 @@
+import type { RootQuery } from "./graphql";
+import { fetchData, gql } from "./fetcher";
+
+type PostsFetched = {
+    data: Pick<RootQuery, 'posts'>
+}
+
+export const fetchAllPosts = async () => {
+    const res = await fetchData({
+        query: gql`
+           query fetchAllPostsQuery {
+                posts {
+                  nodes {
+                    title
+                    date
+                    excerpt
+                    featuredImage {
+                      node {
+                        sourceUrl
+                      }
+                    }
+                    content
+                  }
+                }
+              } 
+        `
+    })
+
+    const data: PostsFetched = await res.json() as PostsFetched
+    return data.data.posts?.nodes
+}
+
+type PostFetched = {
+    data: Pick<RootQuery, 'post'>
+}
+
+export const fetchPost = async (id: string) => {
+    const res = await fetchData({
+        query: gql`
+query fetchPostQuery($id: ID!) {
+  post(id: $id, idType: URI) {
+    title
+    uri
+    content
+  }
+}
+        `,
+        variables: {
+            id
+        }
+    })
+
+    const data: PostFetched = await res.json() as PostFetched
+    return data.data.post
+}
