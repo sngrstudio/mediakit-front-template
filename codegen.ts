afe32d32e15e3de578472c4ae064567fed4787d5
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-03-27 09:22:10 +0000

    WIP

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,14 @@
-import "dotenv/config"
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import 'dotenv/config'
+import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: `${process.env.ADMIN_ENDPOINT}/wp/graphql`,
   generates: {
-    "src/data/graphql.ts": {
-      plugins: ["typescript"]
-    }
-  }
-};
+    'src/data/graphql.ts': {
+      plugins: ['typescript'],
+    },
+  },
+}
 
-export default config;
+export default config
--- src/data/fetcher.ts
+++ src/data/fetcher.ts
@@ -1,11 +1,18 @@
-const GRAPHQL_ENDPOINT = `${process.env.ADMIN_ENDPOINT}/wp/graphql`
+const GRAPHQL_ENDPOINT = `${import.meta.env.DEV ? import.meta.env.ADMIN_ENDPOINT : process.env.ADMIN_ENDPOINT}/wp/graphql`
 
 export const gql = String.raw
-export const fetchData = async ({ query, variables }: { query: ReturnType<typeof gql>; variables?: Record<string, string> }) => await fetch(GRAPHQL_ENDPOINT, {
+export const fetchData = async ({
+  query,
+  variables,
+}: {
+  query: ReturnType<typeof gql>
+  variables?: Record<string, string>
+}) =>
+  await fetch(GRAPHQL_ENDPOINT, {
     method: 'POST',
-    headers: {'Content-Type':'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-        query,
-        variables
-    })
-})
+      query,
+      variables,
+    }),
+  })
